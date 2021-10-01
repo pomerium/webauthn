7b17fbe1e2bfcf99426af600759471d39de8a7bd
Caleb Doxsey <cdoxsey@pomerium.com>
2021-10-01 13:37:17 -0600

    attestation: implement apple format

---
+++ attestation_statement_apple.go
@@ -0,0 +1,76 @@
+package webauthn
+
+import (
+	"crypto"
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+
+	"github.com/pomerium/webauthn/cose"
+)
+
+// VerifyAppleAttestationStatement verifies that an AttestationObject's attestation statement is valid according to the
+// packed verification procedure.
+func VerifyAppleAttestationStatement(
+	attestationObject *AttestationObject,
+	clientDataJSONHash ClientDataJSONHash,
+) error {
+	certificate, err := attestationObject.Statement.UnmarshalCertificate()
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidAttestationStatement, err)
+	}
+
+	authenticatorData, err := attestationObject.UnmarshalAuthenticatorData()
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidAttestationStatement, err)
+	}
+
+	publicKey, _, err := cose.UnmarshalPublicKey(
+		authenticatorData.AttestedCredentialData.CredentialPublicKey,
+	)
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidAttestationStatement, err)
+	}
+
+	// 2. Concatenate authenticatorData and clientDataHash to form nonceToHash.
+	nonceToHash := concat(attestationObject.AuthData, clientDataJSONHash[:])
+
+	// 3. Perform SHA-256 hash of nonceToHash to produce nonce.
+	nonce := sha256.Sum256(nonceToHash)
+
+	// 4. Verify that nonce equals the value of the extension with OID 1.2.840.113635.100.8.2 in credCert.
+	certificateNonce, err := getCertificateAppleNonce(certificate)
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidAttestationStatement, err)
+	}
+	if subtle.ConstantTimeCompare(nonce[:], certificateNonce) != 1 {
+		return fmt.Errorf("%w: invalid nonce", ErrInvalidAttestationStatement)
+	}
+
+	// 5. Verify that the credential public key equals the Subject Public Key of credCert.
+	err = verifyAppleAttestationStatementCredentialPublicKey(
+		certificate.PublicKey,
+		publicKey,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func verifyAppleAttestationStatementCredentialPublicKey(
+	certificatePublicKey, credentialPublicKey crypto.PublicKey,
+) error {
+	withEqual, ok := certificatePublicKey.(interface {
+		Equal(x crypto.PublicKey) bool
+	})
+	if !ok {
+		return fmt.Errorf("%w: unsupported key type: %T", ErrInvalidAttestationStatement, certificatePublicKey)
+	}
+
+	if withEqual.Equal(credentialPublicKey) {
+		return fmt.Errorf("%w: mismatched public keys", ErrInvalidAttestationStatement)
+	}
+	return nil
+}
--- certificate.go
+++ certificate.go
@@ -7,12 +7,15 @@ import (
 )
 
 var (
+	errInvalidAppleNonce    = errors.New("invalid apple nonce")
 	errMissingAAGUID        = errors.New("missing AAGUID")
+	errMissingAppleNonce    = errors.New("missing apple nonce")
 	errAAGUIDMarkedCritical = errors.New("AAGUID marked critical")
 )
 
 var (
-	oidAAGUID = asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 45724, 1, 1, 4}
+	oidAAGUID     = asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 45724, 1, 1, 4}
+	oidAppleNonce = asn1.ObjectIdentifier{1, 2, 840, 113635, 100, 8, 2}
 )
 
 func getCertificateAAGUID(certificate *x509.Certificate) (AAGUID, error) {
@@ -30,4 +33,24 @@ func getCertificateAAGUID(certificate *x509.Certificate) (AAGUID, error) {
 		}
 	}
 	return AAGUID{}, errMissingAAGUID
+}
+
+func getCertificateAppleNonce(certificate *x509.Certificate) ([]byte, error) {
+	type appleAnonymousAttestation struct {
+		Nonce []byte `asn1:"tag:1,explicit"`
+	}
+
+	for _, extension := range certificate.Extensions {
+		if !extension.Id.Equal(oidAppleNonce) {
+			continue
+		}
+
+		var value appleAnonymousAttestation
+		_, err := asn1.Unmarshal(extension.Value, &value)
+		if err != nil {
+			return nil, errInvalidAppleNonce
+		}
+		return value.Nonce, nil
+	}
+	return nil, errMissingAppleNonce
 }
