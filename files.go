fbd959780e0cc2167566dac9ab01c449d26c47f8
Caleb Doxsey <cdoxsey@pomerium.com>
2022-08-15 08:25:13 -0600

    fix gofmt

--- files.go
+++ files.go
@@ -4,5 +4,6 @@ import _ "embed"
 
 // AppleWebAuthnRootCAPEM is the Apple WebAuthn Root CA taken from:
 // https://www.apple.com/certificateauthority/Apple_WebAuthn_Root_CA.pem
+//
 //go:embed files/Apple_WebAuthn_Root_CA.pem
 var AppleWebAuthnRootCAPEM []byte
